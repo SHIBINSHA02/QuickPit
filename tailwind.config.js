96acd757480e135b8280a737c3006d1679085690
SHIBINSHA <shibin24888@gmail.com>
2025-06-05 15:24:54 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Your main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // All JS, TS, JSX, TSX files within the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
